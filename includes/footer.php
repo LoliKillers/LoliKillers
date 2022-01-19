44a493efd834e1a2f5cdc76896cbd6900d9ff161
Loli Killers <80396688+LoliKillers@users.noreply.github.com>
2022-01-19 17:49:37 +0700

    Delete includes directory

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer">
-<div class="d-sm-flex justify-content-center justify-content-sm-between">
-<span class="text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://github.com/LoliKillers" target="_blank">Loli Killers</a> 2020</span>
-<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">I Am <a href="https://www.instagram.com/ariasu.xyz" target="_blank">ariasu.xyz </a>Rublix Site</span>
-</div>
-</footer>
